37925a9ec553befeed6886bfd510c98a3eb016e7
NikitaTurlyko <nikita.turlyko@gmail.com>
2020-05-23 14:22:08 +0300

    add project structure

---
+++ force-app/main/default/aura/expenseTrackerApp/expenseTrackerAppController.js
@@ -0,0 +1,15 @@
+({
+    doInit: function(component, event, helper) {
+        const action = component.get("c.getTransactions");
+        action.setCallback(this, function(response) {
+            const state = response.getState();
+            if (state === "SUCCESS") {
+                component.set("v.transactions", response.getReturnValue());
+                console.log(response.getReturnValue())
+            } else {
+                console.log("Failed with state: " + state);
+            }
+        });
+        $A.enqueueAction(action);
+    },
+})
