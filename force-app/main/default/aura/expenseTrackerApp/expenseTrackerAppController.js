6da1753efe44f75914a3ccdac1edab934809d829
NikitaTurlyko <nikita.turlyko@gmail.com>
2020-05-24 14:38:05 +0300

    add spinner, format all code with prettier (tabs)

--- force-app/main/default/aura/expenseTrackerApp/expenseTrackerAppController.js
+++ force-app/main/default/aura/expenseTrackerApp/expenseTrackerAppController.js
@@ -1,18 +1,17 @@
 ({
     doInit: function(component, event, helper) {
-        const action = component.get("c.getTransactions");
+        const action = component.get('c.getTransactions');
         action.setCallback(this, function(response) {
             const state = response.getState();
-            if (state === "SUCCESS") {
-                component.set("v.transactions", response.getReturnValue());
-                console.log(response.getReturnValue())
+            if (state === 'SUCCESS') {
+                component.set('v.transactions', response.getReturnValue());
+                component.set('v.isLoading', false);
+                console.log('loading', component.get('v.isLoading'));
             } else {
-                console.log("Failed with state: " + state);
+                console.log('Failed with state: ' + state);
             }
         });
         $A.enqueueAction(action);
     },
-    handleSectionToggle: function (cmp, event) {
-
-    },
-})
+    handleSectionToggle: function(cmp, event) {}
+});
